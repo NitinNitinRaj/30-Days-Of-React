48f7d7f67319ce3dab6a12bea0e6781110a9b64b
NitinNitinRaj <bnitinraj@gmail.com>
2024-01-14 14:26:22 +0530

    day 24 project done

--- 24_projects/countries-data/src/App.js
+++ 24_projects/countries-data/src/App.js
@@ -3,6 +3,7 @@ import { getCountriesData } from "./apis/country-api";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import BarChart from "./components/BarChart";
 
 export default function App() {
   const [countries, setCountries] = useState([]);
@@ -11,15 +12,79 @@ export default function App() {
 
   useEffect(() => {
     getCountriesData()
-      .then((response) => setCountries(response.data))
+      .then((response) => {
+        setCountries(
+          response.data.sort((a, b) => {
+            const nameA = a.name.common.toUpperCase();
+            const nameB = b.name.common.toUpperCase();
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
+            return 0;
+          })
+        );
+      })
       .catch((err) => setCountries([]));
   }, []);
+
+  const getTop10MostPopulatedCountry = () => {
+    let top10MostPopulatedCountry = countries
+      .sort((a, b) => b.population - a.population)
+      .slice(0, 10);
+
+    const totalPopulation = countries.reduce(
+      (prev, curr) => prev + curr.population,
+      0
+    );
+
+    top10MostPopulatedCountry = top10MostPopulatedCountry.map((country) => {
+      return {
+        name: country.name.common,
+        value: country.population,
+      };
+    });
+
+    top10MostPopulatedCountry.unshift({
+      name: "Total",
+      value: totalPopulation,
+    });
+    return top10MostPopulatedCountry;
+  };
+
+  const getTop10MostSpokenLanguage = () => {
+    const langFreq = {};
+    let top10MostSpokenLanguage = countries.map((country) => {
+      if (country.languages) {
+        const languages = Object.values(country.languages);
+        languages.forEach((language) => {
+          langFreq[language] = (langFreq[language] || 0) + 1;
+        });
+      }
+    });
+    const languages = Object.entries(langFreq).map(([name, value]) => ({
+      name: name,
+      value: value,
+    }));
+    top10MostSpokenLanguage = languages
+      .sort((a, b) => b.value - a.value)
+      .slice(0, 10);
+    return top10MostSpokenLanguage;
+  };
 
   return (
     <div>
       <Header noOfCountries={countries.length} jumpRefTop={jumpRefTop} />
       <Body countries={countries} useRef={jumpRef} />
-      <Footer setRef={jumpRef} jumpRefTop={jumpRefTop} />
+
+      <Footer
+        setRef={jumpRef}
+        jumpRefTop={jumpRefTop}
+        top10MostPopulatedCountry={getTop10MostPopulatedCountry()}
+        top10MostSpokenLanguage={getTop10MostSpokenLanguage()}
+      />
     </div>
   );
 }
--- 24_projects/countries-data/src/components/Footer.js
+++ 24_projects/countries-data/src/components/Footer.js
@@ -1,13 +1,57 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaArrowAltCircleUp } from "react-icons/fa";
+import BarChart from "./BarChart";
 
-export default function Footer({ setRef, jumpRefTop }) {
+export default function Footer({
+  setRef,
+  jumpRefTop,
+  top10MostPopulatedCountry,
+  top10MostSpokenLanguage,
+}) {
+  const [chart, setChart] = useState("population");
+
   const divRef = useRef(null);
+
   useEffect(() => {
     setRef.current = divRef.current;
-  }, [setRef]);
+  }, [setRef, chart]);
+
+  const handleButtonClick = (type) => {
+    setChart(type);
+  };
+
+  let buttonStyle = "px-6 py-2 bg-[#ffa500] rounded";
+
   return (
     <div ref={divRef} className="my-8">
+      <div className="main-container-chart p-4 shadow-xl mb-8">
+        <div className="flex justify-center items-center">
+          <button
+            className={`${buttonStyle} mr-6`}
+            onClick={() => handleButtonClick("population")}
+          >
+            POPULATION
+          </button>
+          <button
+            className={buttonStyle}
+            onClick={() => handleButtonClick("language")}
+          >
+            LANGUAGES
+          </button>
+        </div>
+        <p className="text-center mt-4 text-lg">
+          {chart === "population"
+            ? "10 Most populated countries in the world"
+            : "10 Most spoken languages in the world"}
+        </p>
+        <BarChart
+          data={
+            chart === "population"
+              ? top10MostPopulatedCountry
+              : top10MostSpokenLanguage
+          }
+        />
+      </div>
       <div className="bg-[#f0f0f0] py-8 text-center">
         <p className="text-[#585555] text-xl font-semibold pb-4">
           {" "}
